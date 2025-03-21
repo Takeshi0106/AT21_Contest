f27b3d9a10a8be65fdfcce0ac2ebaf30e840bcfd
Takeshi <matsuda.takeshi.30692@gmail.com>
2025-03-22 06:06:13 +0900

    カメラスクリプトを作成中
    カメラブランチを作成して引き継ぐ

---
+++ Contest/Assets/Object/CameraScrept/TPFCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Cameraコンポーネントが必須！！
+[RequireComponent(typeof(Camera))]
+
+public class TPFCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
