b3629867bb86d07a028c027f3e28ae8374fae914
Takeshi <matsuda.takeshi.30692@gmail.com>
2025-04-28 11:41:50 +0900

    デバッグログ追加

--- Contest/Assets/Object/Player/State/DerivedState/PlayerJumpState.cs
+++ Contest/Assets/Object/Player/State/DerivedState/PlayerJumpState.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // =============================
@@ -43,7 +41,9 @@ public class PlayerJumpState : StateClass<PlayerState>
     {
         if (!currentState.GetPlayerAirFlag())
         {
+#if UNITY_EDITOR
             Debug.Log("ジャンプ");
+#endif
 
             // ジャンプ処理
             currentState.GetPlayerRigidbody().AddForce(new Vector3(0.0f, currentState.GetPlayerJumpPower(), 0.0f), ForceMode.Impulse);
