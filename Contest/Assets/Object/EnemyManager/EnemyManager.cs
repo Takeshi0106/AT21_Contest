450adae877cf1a8a7d6024b1dccaa0d5714dc5b1
Takeshi <matsuda.takeshi.30692@gmail.com>
2025-05-12 12:12:01 +0900

    修正

---
+++ Contest/Assets/Object/EnemyManager/EnemyManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+// ===============================
+// EnemyManager
+// 当たったらEnemyを呼び出す
+// ===============================
+
+public class EnemyManager : MonoBehaviour
+{
+    private List<EnemyState> enemies = new List<EnemyState>();
+    public UnityEvent<float> onEnemySlow;
+    EnemySystem enemyManager = null;
+    int cnt = 0;
+
+    [Header("プレイヤーのタグ名")]
+    private string playerTag = "Player";
+    [Header("敵のsystem")]
+    private EnemySystem system = null;
+
+
+    // Enemyを数を取得
+    public void RegisterEnemy(EnemyState enemy)
+    {
+        enemies.Add(enemy);
+        // enemy.gameObject.SetActive(false);
+
+        
+
+        if (cnt == 0)
+        {
+            // EnemySystemに自分を渡す
+            enemyManager = system.GetComponent<EnemySystem>();
+            enemyManager.RegisterEnemyManager(this);
+            
+            cnt = 1;
+        }
+    }
+
+
+    // 敵を倒したときの処理
+    public void UnregisterEnemy(EnemyState enemy)
+    {
+        enemies.Remove(enemy);
+
+        if (enemies.Count == 0)
+        {
+            // 敵が倒されたらEnemySystemに知らせる
+            enemyManager.UnregisterEnemyManager(this);
+        }
+    }
+
+
+    // セットされているエネミーを遅くする
+    public void EnemySlow(float slowSpead)
+    {
+        Debug.Log("呼び出された");
+        onEnemySlow.Invoke(slowSpead);
+    }
+
+
+
+    public void AddOnEnemySlow(UnityAction<float> action)
+    {
+        onEnemySlow.AddListener(action);
+    }
+
+    // プレイヤーが敵にぶつかった時の処理
+    void OnTriggerEnter(Collider other)
+    {
+        MultiTag tag = other.GetComponent<MultiTag>();
+
+        // 配列の中に同じものがあるかのチェック
+        if (tag.HasTag(playerTag))
+        {
+            foreach (EnemyState enemy in enemies)
+            {
+                enemy.gameObject.SetActive(true);  // 登録された敵を有効化
+            }
+        }
+    }
+
+
+
+    // プレイヤーが敵と離れた時の処理
+    void OnTriggerExit(Collider other)
+    {
+        MultiTag tag = other.GetComponent<MultiTag>();
+
+        // 配列の中に同じものがあるかのチェック
+        if (tag.HasTag(playerTag))
+        {
+            foreach (EnemyState enemy in enemies)
+            {
+                enemy.gameObject.SetActive(false);  // 登録された敵を有効化
+            }
+        }
+    }
+
+}
---
+++ Contest/Assets/Object/EnemyManager/EnemySystem.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+// =====================================
+// EnemyManagerを管理して、resultsceneを呼ぶ
+// ======================================
+
+public class EnemySystem : MonoBehaviour
+{
+    private List<EnemyManager> enemyMan = new List<EnemyManager>();
+    public UnityEvent<float> onEnemySlow;
+
+
+    // EnemyManagerの数を取得
+    public void RegisterEnemyManager(EnemyManager enemy)
+    {
+        enemyMan.Add(enemy);
+    }
+
+
+    // resultsceneに移行
+    private void LoadResultScene()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        SceneManager.LoadScene("ResultScene");
+    }
+
+
+    // EnemyMangaerの全ての敵を倒したときの処理
+    public void UnregisterEnemyManager(EnemyManager enemy)
+    {
+        enemyMan.Remove(enemy);
+
+        if (enemyMan.Count == 0)
+        {
+            Debug.Log("すべての敵を倒しました！");
+            LoadResultScene();
+        }
+    }
+
+
+    // セットされているエネミーを遅くする
+    public void EnemySlow(float slowSpead)
+    {
+        foreach (EnemyManager enemy in enemyMan)
+        {
+            enemy.EnemySlow(slowSpead);
+        }
+    }
+}
